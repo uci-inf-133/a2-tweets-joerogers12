7d9011952bb7e0433d27d72dc5a6c962b713bf72
Joe Rogers <frogers@uci.edu>
2025-10-28 22:27:57 -0700

    Add min and max date calculation and set them as inner text on appropriate tags

--- js/about.js
+++ js/about.js
@@ -12,6 +12,21 @@ function parseTweets(runkeeper_tweets) {
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
 	document.getElementById('numberTweets').innerText = tweet_array.length;	
+
+	// Find the min and max dates of the tweets
+	const minDate = new Date(
+		Math.min(...runkeeper_tweets.map((tweet) => {
+			return new Date(tweet.created_at);
+		}))
+	);
+	let maxDate = new Date(
+		Math.max(...runkeeper_tweets.map((tweet) => {
+			return new Date(tweet.created_at);
+		}))
+	);
+
+	document.getElementById('firstDate').innerText = minDate.toLocaleDateString();
+	document.getElementById('lastDate').innerText = maxDate.toLocaleDateString();
 }
 
 //Wait for the DOM to load
